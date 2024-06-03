b7a4abc564c2aebb1d1732b5d680088a897c6a57
josetor101 <josetordecilla800@gmail.com>
2024-06-03 13:57:31 -0500

    Progress in exploring-img options [vanishing card, component setup for info]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import { useState, useRef } from 'react';
 import './App.css';
-import ArrowDownward from '@mui/icons-material/ArrowDownward';
-import Rope from './img/rope.svg';
-import ClothingPin from './img/clothin_pin.svg';
-import Header from './components/Header';
-
+import { useState, useRef } from 'react';
 import { useInView } from "react-intersection-observer";
+
+// COMPONENTS
+import Header from './components/Header';
+import Photo from './components/Photo';
+
+// IMG's / ICONS
+import ArrowDownward from '@mui/icons-material/ArrowDownward';
+import ClearIcon from '@mui/icons-material/Clear';
+import Rope from './img/rope.svg';
+import Test from './img/test.jpg';
+//import { Photo } from '@mui/icons-material';
 
 function App() {
   const[seeHeader, setSeeHeader] = useState(false);
-
+  const[details, setDetails] = useState(false);
   //const exploringOptionsRef = useRef(null);
 
   const handleIntersection = (inView) => {
     setSeeHeader(inView);
   };
 
+
   const {ref, inView} = useInView({
     threshold: 0.7,
     onChange: handleIntersection,
   });
+
+  const showDetails = (val) =>{
+    setDetails(val);
+  }
 
   return (
     <div className='main-wrapper'>
@@ -29,8 +40,6 @@ function App() {
       </header>
 
       <div className="landing">
-
-          
 
           <div className='text-intro'>
               <p>¿Por qué viajar con nosotros?</p>
@@ -46,24 +55,37 @@ function App() {
           <p>Explora el Suroeste antioqueño</p>
         </div>
         
-        <div className='carousel-container'>
+        <div className={details ?  'carousel-container-hidden' : 'carousel-container'}>
           <figure id='rope-hanging1'>
-            <img src={Rope} alt="rope"></img>
+            <img src={Rope} alt="rope" key={"rope"}></img>
           </figure>
-          <div className='photo-holder'>
+          <div className='photo-container'>
 
-            <div className="pin-container">
-                <img src={ClothingPin} className='clothin-pin' alt="clothin-pin"></img>
-            </div>
-
-            <div className='photo-container'>
-                <div className='square'>
-            </div>
-
-            </div>
+              {Array.from({ length: 4 }).map((_, index) => (
+                /* Aquí se puede renderizar cada foto con información de un JSON*/
+                <Photo key={index} inview={inView ? 1 : 0} showDetails={showDetails}/>
+              ))}
           </div>
         </div>
 
+        <div className={details ? 'exploring-detail-c visible' : 'exploring-detail-c'}>
+          <div className='square-info-c'>
+            <div className='detail-img'>
+              <img src={Test} alt='test-img'></img>
+            </div>
+            <div className='detail-info-c'>
+              <div className='detail-title'> 
+                <p>Cerro San Nicolás</p>
+                <ClearIcon sx={{ fontSize: 30, strokeWidth: 3 }} className='exit-icon' onClick={() => showDetails(false)}/>
+                {/*<button onClick={() => showDetails(false)}>Volver</button>*/}
+              </div>
+              <div className=''>Altura: 4080 MSNM</div>
+              <div className=''>Dificultad: *****</div>
+              <div className='detail-desc'><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in semper felis. Sed gravida ullamcorper lectus ac venenatis. Praesent nulla ligula, condimentum sit amet dolor a, ullamcorper ornare libero. Nam pharetra sodales dui, non interdum lorem viverra a.</p></div>
+              
+            </div>
+          </div>
+        </div>
 
       </div>  
     </div>
---
+++ src/components/Photo.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import ClothingPin from '../img/clothin_pin.svg';
+import Test from '../img/test.jpg';
+
+function Photo ({ inview, showDetails }) {
+    const [clicked, setClicked] = useState(false);
+    const visibilityClass = inview === 1 ? 'bounce-in' : 'hidden';
+    
+    const handleClick = () => {
+        setClicked(true);
+    }
+
+    useEffect(() => {
+        if (clicked) {
+            showDetails(true);
+            setClicked(false);
+        }
+    }, [clicked, showDetails]);
+
+    return (
+        <div className={`photo-holder ${visibilityClass}`} onClick={handleClick}>
+            <img src={ClothingPin} className='clothin-pin' alt="clothin-pin" />
+            <div className='square'>
+                <div className='square-photo'>
+                    <img src={Test} alt='test-img' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Photo;
